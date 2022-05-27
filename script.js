54d9ce2c2c670cca6974f8107782037871cb16db
Ritkovsky <96311774+Ritkovsky@users.noreply.github.com>
2022-05-27 02:50:57 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function Backgroundchange()
-{
-    let heroText = document.getElementById("hero-text");
-    heroText.innerHTML = "Poznaj nas!";
-}
-function Start()
-{
-    setTimeout(Backgroundchange, 1000)
-}
